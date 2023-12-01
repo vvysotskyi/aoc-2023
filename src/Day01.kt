2b0d3d3b8f2ee37fe8fcdc85bf6775dad1eb936c
Volodymyr Vysotskyi <vvovyk@gmail.com>
2023-12-01 08:51:16 +0200

    Day 1: Trebuchet?!

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,50 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf { line ->
+            val first = line.toCharArray().find { it.isDigit() }!!.digitToInt()
+            val last = line.toCharArray().findLast { it.isDigit() }!!.digitToInt()
+            first * 10 + last
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val wordToNum = mapOf(
+            "one" to 1,
+            "two" to 2,
+            "three" to 3,
+            "four" to 4,
+            "five" to 5,
+            "six" to 6,
+            "seven" to 7,
+            "eight" to 8,
+            "nine" to 9
+        )
+
+        val allNums = wordToNum.keys.toMutableSet()
+        (1..9).forEach { allNums += it.toString() }
+
+        return input.sumOf { line ->
+            val first = allNums.minBy { line.indexOfOrLen(it) }
+            val last = allNums.maxBy { line.lastIndexOf(it) }
+            val firstNum = wordToNum[first] ?: first.toInt()
+            val lastNum = wordToNum[last] ?: last.toInt()
+
+            firstNum * 10 + lastNum
+        }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    var testInput = readInput("Day01_test1")
+    check(part1(testInput) == 142)
+    testInput = readInput("Day01_test2")
+    check(part2(testInput) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+private fun CharSequence.indexOfOrLen(string: String): Int {
+    val indexOf = indexOf(string)
+    return if (indexOf >= 0) indexOf else length
 }
